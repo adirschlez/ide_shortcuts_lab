bebeb7578696cd92bd8a2f66cac9671bdf410ee8
adirschlez <adir.schlezinger@axonius.com>
2024-07-30 10:14:56 +0300

    typos!!!

--- postfix.py
+++ postfix.py
@@ -13,15 +13,15 @@ def fib(n):
 
 
 def run():
-    itterations = 200
-    # We whould like to add a shortcut for printing this struct of code:
+    iterations = 200
+    # We would like to add a shortcut for printing this struct of code:
     #
     # start = time.time()
-    # fib(itterations)
+    # fib(iterations)
     # end = time.time()
-    # print(f"Function fib(itterations) Execution time: {end - start}")
+    # print(f"Function fib(iterations) Execution time: {end - start}")
 
-    fib(itterations)
+    fib(iterations)
 
     # We will define a new POSTFIX COMPLETION for this action
     # GO TO SETTINGS -> POSTFIX COMPLETION and define time postfix action
